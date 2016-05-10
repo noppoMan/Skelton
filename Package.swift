914a27faa4540c09a9595c262be6a7df545a12d6
noppoman <yuki@miketokyo.com>
2016-05-11 02:07:49 +0900

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Skelton",
     dependencies: [
-        .Package(url: "https://github.com/noppoMan/Suv.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/noppoman/HTTPParser", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/noppoMan/HTTP.git", majorVersion: 0, minor: 5)
+        .Package(url: "https://github.com/noppoMan/Suv.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/noppoman/HTTPParser", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/noppoMan/HTTP.git", majorVersion: 0, minor: 7)
     ]
 )
