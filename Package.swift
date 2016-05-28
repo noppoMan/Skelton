bcc0fa06399393cf4be69a55db56c9ee31a5bc4f
noppoman <yuki@miketokyo.com>
2016-05-28 10:25:47 +0900

    Fix repo name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Skelton",
     dependencies: [
         .Package(url: "https://github.com/noppoMan/Suv.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/noppoman/HTTPParser", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/noppoMan/HTTPParser.git", majorVersion: 0, minor: 8),
         .Package(url: "https://github.com/noppoMan/HTTP.git", majorVersion: 0, minor: 7)
     ]
 )
