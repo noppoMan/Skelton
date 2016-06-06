c6e25505b30dddb733fb2fa512fc801c3f4f76fb
noppoman <yuki@miketokyo.com>
2016-06-07 01:12:26 +0900

    Bump Suv version to 0.6

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Skelton",
     dependencies: [
-        .Package(url: "https://github.com/noppoMan/Suv.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/noppoMan/Suv.git", majorVersion: 0, minor: 6),
         .Package(url: "https://github.com/noppoMan/HTTPParser.git", majorVersion: 0, minor: 8),
         .Package(url: "https://github.com/noppoMan/HTTP.git", majorVersion: 0, minor: 7)
     ]
